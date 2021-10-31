9cfc7866083c39738a9161e5aa1781f115ee4c59
mag-d-alen <59833104+mag-d-alen@users.noreply.github.com>
2021-10-31 19:22:15 +0200

    adding search function results displaying results adding loader

---
+++ constants.js
@@ -0,0 +1,5 @@
+/** @format */
+
+const RESULTS_URL =
+  'https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/';
+const LIMIT = '10';
---
+++ index.js
@@ -0,0 +1,40 @@
+/** @format */
+
+const fetchResults = async () => {
+  const query = document.getElementById('query-input').value;
+  const loader = document.getElementById('spinner');
+  spinner.classList.remove('hidden');
+  try {
+    const res = await fetch(
+      `${RESULTS_URL}search?query=${query}&limit=${LIMIT}&exchange=NASDAQ`
+    );
+    const data = await res.json();
+    displayResults(data);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    spinner.classList.add('hidden');
+  }
+};
+const displayResults = (data) => {
+  const results = document.getElementById('results');
+  data.map((company) => {
+    const result = document.createElement('div');
+    const resultLink = document.createElement('a');
+    resultLink.setAttribute('href', `./company.html?symbol=${company.symbol}`);
+    const symbolDiv = document.createElement('div');
+    const nameDiv = document.createElement('div');
+    nameDiv.textContent = company.name;
+    symbolDiv.textContent = company.symbol;
+    resultLink.prepend(symbolDiv, nameDiv);
+    result.append(resultLink);
+    symbolDiv.classList.add('symbol-div');
+    result.classList.add('result');
+    resultLink.classList.add('result-link');
+    results.appendChild(result);
+  });
+};
+
+(() => {
+  document.getElementById('search').addEventListener('click', fetchResults);
+})();
