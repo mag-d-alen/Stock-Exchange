d701a58d250d256d0dbd8419916483d61a2cd445
mag-d-alen <59833104+mag-d-alen@users.noreply.github.com>
2021-11-03 12:57:14 +0200

    M3.1 optimizing fetch with multiple symbols

--- js/index.js
+++ js/index.js
@@ -18,70 +18,80 @@ const fetchResults = async () => {
       `${RESULTS_URL}search?query=${query}&limit=${LIMIT}&exchange=NASDAQ`
     );
     const data = await res.json();
-    displayResults(data);
+    fetchExtraData(data);
   } catch (error) {
     console.log(error);
   } finally {
     spinner.classList.add('hidden');
   }
 };
-const displayResults = (data) => {
-  const results = document.getElementById('results');
-  data.map((company, i) => {
-    const result = document.createElement('div');
-    const resultLink = document.createElement('a');
-    result.id = i;
-    resultLink.setAttribute('href', `${SYMBOL_SUFFIX}${company.symbol}`);
-    resultLink.target = '_blank';
-    const symbolDiv = document.createElement('div');
-    const nameDiv = document.createElement('div');
-    nameDiv.innerHTML = highlight(company.name);
-    symbolDiv.innerHTML = highlight(company.symbol);
-    resultLink.prepend(symbolDiv, nameDiv);
-    result.append(resultLink);
-    symbolDiv.classList.add('symbol-div');
-    result.classList.add('result');
-    resultLink.classList.add('result-link');
 
-    results.appendChild(result);
-    fetchExtraData(result.id, company.symbol);
-  });
-};
-
-const fetchExtraData = async (id, symbol) => {
+const fetchExtraData = async (data) => {
+  const symbols = [];
+  data.forEach((result) => symbols.push(result.symbol));
   try {
-    const result = document.getElementById(id);
-    const profileImg = document.createElement('img');
-    profileImg.classList.add('profile-img');
-    const res = await fetch(`${COMP_URL}${symbol}`);
-    const data = await res.json();
-
-    if (data.profile) {
-      let { changesPercentage, image } = data.profile;
-      image != undefined
-        ? (profileImg.src = image)
-        : (profileImg.src = '../images/noDataIcon.jpg');
-      profileImg.setAttribute('onerror', `this.src='../images/noDataIcon.jpg'`);
-
-      const percentChange = document.createElement('div');
-      percentChange.innerText = changesPercentage;
-      percentChange.classList.add('profile-changes');
-      if (changesPercentage !== '(0%)') {
-        const percentShorten = Number(changesPercentage).toFixed(3);
-        percentChange.innertext = `${percentShorten} %`;
-        percentShorten > 0
-          ? percentChange.classList.add('green')
-          : percentChange.classList.add('red');
-      } else {
-        percentChange.innerHTML = 'No data on recent price changes';
-      }
-      result.append(percentChange);
-    } else {
-      profileImg.src = './images/noDataIcon.jpg';
-    }
-    result.prepend(profileImg);
+    const res = await Promise.all([
+      fetch(`${COMP_URL}${symbols.splice(0, 3)}`),
+      fetch(`${COMP_URL}${symbols.splice(0, 3)}`),
+      fetch(`${COMP_URL}${symbols.splice(0, 3)}`),
+      fetch(`${COMP_URL}${symbols.splice(0, 3)}`),
+    ])
+      .then((resps) => Promise.all(resps.map((res) => res.json())))
+      .then((data) =>
+        data.map((item) =>
+          item.companyProfiles
+            ? item.companyProfiles.forEach((profile) => displayCompany(profile))
+            : displayCompany(item)
+        )
+      );
   } catch (error) {
     console.log(error);
+  }
+};
+
+const displayCompany = (company) => {
+  const results = document.getElementById('results');
+  const result = document.createElement('div');
+  const resultLink = document.createElement('a');
+  const profileImg = document.createElement('img');
+  const symbolDiv = document.createElement('div');
+  const nameDiv = document.createElement('div');
+
+  resultLink.setAttribute('href', `${SYMBOL_SUFFIX}${company.symbol}`);
+  resultLink.target = '_blank';
+  nameDiv.innerHTML = highlight(company.profile.companyName);
+  symbolDiv.innerHTML = highlight(company.symbol);
+  symbolDiv.classList.add('symbol-div');
+  result.classList.add('result');
+  resultLink.classList.add('result-link');
+
+  profileImg.classList.add('profile-img');
+  if (company.profile) {
+    let { changesPercentage, image } = company.profile;
+    image != undefined
+      ? (profileImg.src = image)
+      : (profileImg.src = '../images/noDataIcon.jpg');
+    profileImg.setAttribute('onerror', `this.src='../images/noDataIcon.jpg'`);
+    resultLink.prepend(symbolDiv, nameDiv);
+    result.append(resultLink);
+    result.prepend(profileImg);
+    results.appendChild(result);
+
+    const percentChange = document.createElement('div');
+    percentChange.innerText = changesPercentage;
+    percentChange.classList.add('profile-changes');
+    if (changesPercentage !== '(0%)') {
+      const percentShorten = Number(changesPercentage).toFixed(3);
+      percentChange.innertext = `${percentShorten} %`;
+      percentShorten > 0
+        ? percentChange.classList.add('green')
+        : percentChange.classList.add('red');
+    } else {
+      percentChange.innerHTML = 'No data on recent price changes';
+    }
+    result.append(percentChange);
+  } else {
+    profileImg.src = './images/noDataIcon.jpg';
   }
 };
 
