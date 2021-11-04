feeb75861ddde7ba4cbf279d036aeaf260b980ff
mag-d-alen <59833104+mag-d-alen@users.noreply.github.com>
2021-11-04 11:44:59 +0200

    M9 refactoring the company page into a class

--- js/company.js
+++ js/company.js
@@ -1,133 +1,137 @@
 /** @format */
 
-const urlParams = new URLSearchParams(window.location.search);
-const symbol = urlParams.get('symbol');
-
-const makeElements = () => {
-  const wrapper = document.getElementById('company-content-wrapper');
-  const company = document.createElement('div');
-  const compPrice = document.createElement('div');
-  const compImage = document.createElement('img');
-  const name = document.createElement('div');
-  const compDescription = document.createElement('div');
-  const link = document.createElement('a');
-  const changes = document.createElement('div');
-  const graph = document.createElement('section');
-  const container = document.createElement('div');
-  const myChart = document.createElement('canvas');
-
-  company.id = 'company';
-  compPrice.id = 'compPrice';
-  compImage.id = 'compImage';
-  name.id = 'name';
-  compDescription.id = 'compDescription';
-  link.id = 'link';
-  changes.id = 'changes';
-  myChart.id = 'myChart';
-
-  link.textContent = 'Visit our site';
-  link.target = '_blank';
-
-  company.classList.add('company');
-  compImage.classList.add('company-img');
-  name.classList.add('company-name');
-  compDescription.classList.add('company-description');
-  link.classList.add('company-link');
-  compPrice.classList.add('company-price');
-  compDescription.classList.add('company-description');
-  container.classList.add('container');
-  graph.classList.add('graph');
-
-  graph.append(container);
-  container.append(myChart);
-  myChart.getContext('2d');
-
-  company.append(
-    compImage,
-    name,
-    compDescription,
-    link,
-    compPrice,
-    compDescription,
-    changes,
-    graph
-  );
-
-  wrapper.append(company);
-};
-
-const getCompanyProfile = async (symbol) => {
-  const spinner = document.getElementById('spinner');
-  try {
-    spinner.classList.remove('hidden');
-    const res = await fetch(`${COMP_URL}${symbol}`);
-    const data = await res.json();
-    displayCompany(data.profile);
-  } catch (error) {
-    console.error(error);
-  } finally {
-    spinner.classList.add('hidden');
+class Company {
+  constructor(location, symbol) {
+    this.location = location;
+    this.symbol = symbol;
   }
-};
 
-const displayCompany = (comp) => {
-  const { image, companyName, description, website, changesPercentage, price } =
-    comp;
-  const compImg = document.getElementById('compImage');
-  image != undefined
-    ? (compImg.src = image)
-    : (compImg.src = './images/noDataIcon.jpg');
-  compImg.setAttribute('onerror', `this.src='./images/noDataIcon.jpg'`);
-  document.getElementById('name').innerText = companyName;
-  compDescription.innerText = description;
-  compPrice.innerText = `Stock price $${price}`;
-  link.href = website;
-  const changes = document.getElementById('changes');
-  if (changesPercentage !== '(0%)') {
-    const percentShorten = Number(changesPercentage).toFixed(3);
-    changes.innerText = `${percentShorten} %`;
-    percentShorten > 0
-      ? changes.classList.add('changes-green')
-      : changes.classList.add('changes-red');
-    changes.innerHTML = `Recent price change ${percentShorten} %`;
-  } else {
-    changes.innerHTML = 'No data on recent price changes';
+  makeElements() {
+    const wrapper = document.getElementById('company-content-wrapper');
+    const company = document.createElement('div');
+    const compPrice = document.createElement('div');
+    const compImage = document.createElement('img');
+    const name = document.createElement('div');
+    const compDescription = document.createElement('div');
+    const link = document.createElement('a');
+    const changes = document.createElement('div');
+    const graph = document.createElement('section');
+    const container = document.createElement('div');
+    const myChart = document.createElement('canvas');
+
+    company.id = 'company';
+    compPrice.id = 'compPrice';
+    compImage.id = 'compImage';
+    name.id = 'name';
+    compDescription.id = 'compDescription';
+    link.id = 'link';
+    changes.id = 'changes';
+    myChart.id = 'myChart';
+
+    link.textContent = 'Visit our site';
+    link.target = '_blank';
+
+    company.classList.add('company');
+    compImage.classList.add('company-img');
+    name.classList.add('company-name');
+    compDescription.classList.add('company-description');
+    link.classList.add('company-link');
+    compPrice.classList.add('company-price');
+    compDescription.classList.add('company-description');
+    container.classList.add('container');
+    graph.classList.add('graph');
+
+    graph.append(container);
+    container.append(myChart);
+    myChart.getContext('2d');
+
+    company.append(
+      compImage,
+      name,
+      compDescription,
+      link,
+      compPrice,
+      compDescription,
+      changes,
+      graph
+    );
+
+    wrapper.append(company);
   }
-  changes.classList.add('company-changes');
-  fetchHistoricData();
-};
 
-const fetchHistoricData = async () => {
-  try {
-    const res = await fetch(`${HISTORIC_URL}${symbol}${SERIE}`);
-    const data = await res.json();
-    spinner.classList.add('hidden');
-    const dates = [];
-    const prices = [];
-    data.historical.slice(0, 100).map((entry) => dates.push(entry.date));
-    data.historical.slice(0, 100).map((entry) => prices.push(entry.close));
-    const stocksChart = new Chart(myChart, {
-      type: 'bar',
-      data: {
-        labels: dates.reverse(),
-        datasets: [
-          {
-            label: `${symbol} Stock prices`,
-            data: prices.reverse(),
-            borderColor: 'lightgray',
-            backgroundColor: 'darkgray',
-          },
-        ],
-      },
-    });
-  } catch (error) {
-    console.log(error);
-  } finally {
-    spinner.classList.add('hidden');
+  async getCompanyProfile(symbol) {
+    const spinner = document.getElementById('spinner');
+    try {
+      spinner.classList.remove('hidden');
+      const res = await fetch(`${COMP_URL}${symbol}`);
+      const data = await res.json();
+      this.displayCompany(data.profile);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      spinner.classList.add('hidden');
+    }
   }
-};
 
-(() => {
-  makeElements();
-  getCompanyProfile(symbol);
-})();
+  displayCompany(comp) {
+    const {
+      image,
+      companyName,
+      description,
+      website,
+      changesPercentage,
+      price,
+    } = comp;
+    const compImg = document.getElementById('compImage');
+    image != undefined
+      ? (compImg.src = image)
+      : (compImg.src = './images/noDataIcon.jpg');
+    compImg.setAttribute('onerror', `this.src='./images/noDataIcon.jpg'`);
+    document.getElementById('name').innerText = companyName;
+    compDescription.innerText = description;
+    compPrice.innerText = `Stock price $${price}`;
+    link.href = website;
+    const changes = document.getElementById('changes');
+    if (changesPercentage !== '(0%)') {
+      const percentShorten = Number(changesPercentage).toFixed(3);
+      changes.innerText = `${percentShorten} %`;
+      percentShorten > 0
+        ? changes.classList.add('changes-green')
+        : changes.classList.add('changes-red');
+      changes.innerHTML = `Recent price change ${percentShorten} %`;
+    } else {
+      changes.innerHTML = 'No data on recent price changes';
+    }
+    changes.classList.add('company-changes');
+  }
+
+  async fetchHistoricData(symbol) {
+    try {
+      const res = await fetch(`${HISTORIC_URL}${symbol}${SERIE}`);
+      const data = await res.json();
+      spinner.classList.add('hidden');
+      const dates = [];
+      const prices = [];
+      data.historical.slice(0, 100).map((entry) => dates.push(entry.date));
+      data.historical.slice(0, 100).map((entry) => prices.push(entry.close));
+      const stocksChart = new Chart(myChart, {
+        type: 'bar',
+        data: {
+          labels: dates.reverse(),
+          datasets: [
+            {
+              label: `${symbol} Stock prices`,
+              data: prices.reverse(),
+              borderColor: 'lightgray',
+              backgroundColor: 'darkgray',
+            },
+          ],
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      spinner.classList.add('hidden');
+    }
+  }
+}
--- js/marquee.js
+++ js/marquee.js
@@ -1,26 +1,31 @@
 /** @format */
-const marqueeFetch = async () => {
-  try {
-    const res = await fetch(MARQUEE_URL);
-    const data = await res.json();
-    createMarquee(data);
-  } catch (error) {
-    console.error(error.message);
-  }
-};
 
-const createMarquee = (data) => {
-  const marquee = document.getElementById('marquee');
-  data.slice(0, 1500).map((item) => {
-    const marqueeItem = document.createElement('div');
-    const itemPrice = document.createElement('span');
-    marqueeItem.classList.add('marquee-item');
-    marqueeItem.innerText = item.symbol;
-    itemPrice.innerText = item.price;
-    Number(item.price) > 0
-      ? itemPrice.classList.add('green')
-      : itemPrice.classList.add('red');
-    marqueeItem.append(itemPrice);
-    marquee.append(marqueeItem);
-  });
-};
+class Marquee {
+  constructor(marquee) {
+    this.marquee = marquee;
+  }
+
+  async fetchMarquee() {
+    try {
+      const res = await fetch(MARQUEE_URL);
+      const data = await res.json();
+      this.createMarquee(data);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+  createMarquee(data) {
+    data.slice(0, 1500).map((item) => {
+      const marqueeItem = document.createElement('div');
+      const itemPrice = document.createElement('span');
+      marqueeItem.classList.add('marquee-item');
+      marqueeItem.innerText = item.symbol;
+      itemPrice.innerText = item.price;
+      Number(item.price) > 0
+        ? itemPrice.classList.add('green')
+        : itemPrice.classList.add('red');
+      marqueeItem.append(itemPrice);
+      this.marquee.append(marqueeItem);
+    });
+  }
+}
